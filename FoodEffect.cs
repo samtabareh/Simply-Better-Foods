db524fd455ebd12fa3f94310ee55604ca9bd929c
samtabareh <sam.tabareh@gmail.com>
2023-10-02 16:00:58 +0330

    V0.0.1

--- FoodEffect.cs
+++ FoodEffect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,22 +7,41 @@ namespace SimplyBetterFoodsNS
 {
     public enum FoodEffects
     {
+        /// <summary>
+        /// Only adds the Health Damage and Defence params.
+        /// </summary>
         PermEffect, // Permanent Effect
+        /// <summary>
+        /// 
+        /// </summary>
         TempEffect, // Temporary Effect
+        /// <summary>
+        /// 
+        /// </summary>
         CBEffect, // Chance-Based Effect
+        /// <summary>
+        /// Only uses the EffectTimer param.
+        /// </summary>
         WellFed // Gives Well Fed Status Effect
     }
 
     public class FoodEffect
     {
-        private static int Health;
-        private static int Damage;
-        private static int Defence;
-        private static float AttackSpeed;
-        private static float DebuffChance;
-        private static float EffectTimer;
-
-        public FoodEffect(int Health = 0, int Damage = 0, int Defence = 0, float AttackSpeed = 0, 
+        internal static int Health;
+        internal static int HealthAdded;
+        internal static int Damage;
+        internal static int Defence;
+        internal static int AttackSpeed;
+        internal static float DebuffChance;
+        internal static float EffectTimer;
+        /// <summary>
+        /// </summary>
+        /// <param name="Health">Max Value: 5</param>
+        /// <param name="Damage">Max Value: 5</param>
+        /// <param name="Defence">Max Value: 5</param>
+        /// <param name="AttackSpeed">Max Value: 5</param>
+        /// <param name="EffectChance">Min Value: 0.01f Max Value: 0.99f</param>
+        public FoodEffect(int Health = 0, int Damage = 0, int Defence = 0, int AttackSpeed = 0, 
             float EffectChance = 0, float EffectTimer = 0)
         {
             FoodEffect.Health = Health;
@@ -31,13 +51,34 @@ namespace SimplyBetterFoodsNS
             FoodEffect.DebuffChance = EffectChance;
             FoodEffect.EffectTimer = EffectTimer;
         }
-        public static void PermBuff(Combatable Vill)
+        private static bool PermEffectb = false;
+        /// <summary>
+        /// Only uses Health Damage and Defence args.
+        /// </summary>
+        /// <param name="want">Set it a value if you want a FoodEffects return.</param>
+        internal static FoodEffects PermEffect(bool want)
         {
-            Vill.BaseCombatStats.MaxHealth += Health;
-            Vill.BaseCombatStats.AttackDamage += Damage;
-            Vill.BaseCombatStats.Defence += Defence;
-            Vill.BaseCombatStats.AttackSpeed += AttackSpeed;
-
+            PermEffectb = PermEffectb ? false : true;
+            return FoodEffects.PermEffect;
+        }
+        internal static void PermEffect()
+        {
+            PermEffectb = PermEffectb ? false : true;
+        }
+        [HarmonyPatch(typeof(Combatable))]
+        [HarmonyPatch(nameof(Combatable.ProcessedCombatStats), MethodType.Getter)]
+        [HarmonyPostfix]
+        private static void Combatable__ProcessedCombatStats_Postfix(ref CombatStats __result)
+        {
+            CombatStats stats = new CombatStats();
+            if (Health + HealthAdded <= 5)
+                stats.MaxHealth = Health;
+            HealthAdded += Health;
+            stats.AttackDamage = Damage;
+            stats.Defence = Defence;
+            stats.SpecialHits = new List<SpecialHit>();
+            if (PermEffectb)
+                __result.AddStats(stats);
         }
         public static void WellFed(Combatable Vill)
         {
--- Mod.cs
+++ Mod.cs
@@ -9,14 +9,14 @@ namespace SimplyBetterFoodsNS
         public static SimplyBetterFoods simply = new SimplyBetterFoods();
         public static WorldManager world = new WorldManager();
 
-        internal static List<FoodToEffect> FoodToEffects = new List<FoodToEffect>() { new FoodToEffect(FoodEffects.PermEffect, new FoodEffect(), "fruit_salad") };
+        internal static List<FoodToEffect> FoodToEffects = new List<FoodToEffect>();
         public static void AddFoodEffect(FoodToEffect FoodToEffect)
         {
             FoodToEffects.Add(FoodToEffect);
         }
         [HarmonyPatch(typeof(Food), "ConsumedBy")]
         [HarmonyPostfix]
-        public static void Food__ConsumedBy_Postfix(Combatable ___vill, string ___Id)
+        public static void Food__ConsumedBy_Postfix(Combatable vill, string ___Id)
         {
             Debug.LogWarning($"{simply.Manifest.Name} Consume Patch Applied.");
             for (int i = 0; i < FoodToEffects.Count; i++)
@@ -24,20 +24,22 @@ namespace SimplyBetterFoodsNS
                     switch (FoodToEffects[i].FoodEffects)
                     {
                         case FoodEffects.PermEffect:
-                            FoodEffect.PermBuff(___vill);
+                            FoodEffect.PermEffect();
                             break;
                         case FoodEffects.WellFed:
-                            FoodEffect.WellFed(___vill);
+                            FoodEffect.WellFed(vill);
                             break;
                     }
         }
+
         private void Awake()
         {
             Harmony.PatchAll(typeof(SimplyBetterFoods));
+            Harmony.PatchAll(typeof(FoodEffect));
         }
         public override void Ready()
         {
-            AddFoodEffect(new FoodToEffect(FoodEffects.PermEffect, new FoodEffect());
+            AddFoodEffect(new FoodToEffect(FoodEffect.PermEffect(true), new FoodEffect(1), "apple"));
         }
     }
 }
